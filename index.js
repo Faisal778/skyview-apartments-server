7dfe7c5479916b11a2297ac118ab8d6dd3986632
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-09 10:12:50 +0600

    api endpoint created to get all reservation

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ async function run() {
         const result = await reservationCollection.insertOne(data);
         res.send(result);
     })
+
+    app.get('/reservation', async (req, res)=> {
+        const result = await reservationCollection.find().toArray();
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
